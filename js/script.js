8add2ed4a9e621a18df8d9c5cf0ba677b4098e25
PietroDanieli <1pie7d@gmail.com>
2022-09-21 15:36:15 +0200

    milestone1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
 console.log("Vue app");
 const app = new Vue({
   el: "#app",
-  data: {},
+  data: {
+    emails: [],
+    telephoneNumber: [],
+    loading: true,
+  },
+  mounted() {
+    for (let index = 0; index < 10; index++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((response) => {
+          console.log(response);
+          const { status, data } = response;
+          if (status === 200) {
+            this.emails.push(data.response);
+          }
+        });
+    }
+    loading = false;
+  },
   methods: {},
 });
